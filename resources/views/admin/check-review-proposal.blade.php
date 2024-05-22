b2230b300e19b55d3e784a088c153630b8394158
Syed Alif <syedmuhdalif18@gmail.com>
2024-05-23 07:32:32 +0800

    update on different row

--- resources/views/admin/check-review-proposal.blade.php
+++ resources/views/admin/check-review-proposal.blade.php
@@ -86,15 +86,16 @@
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td class="w-4 p-4">
-                <div class="flex items-center">
-                  <input id="checkbox-table-search-1" type="checkbox"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                  <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                </div>
-              </td>
-              @foreach ($eventProposal as $eventProposalData)
+            @foreach ($eventProposal as $eventProposalData)
+              <tr
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                  <div class="flex items-center">
+                    <input id="checkbox-table-search-1" type="checkbox"
+                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                  </div>
+                </td>
                 <td scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
                   <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image">
                   <div class="ps-3">
@@ -103,21 +104,21 @@
                     {{-- <div class="font-normal text-gray-500 ">{{ $eventProposalData->eventName }}</div> --}}
                   </div>
                   </th>
-              @endforeach
-              <td class="px-6 py-4">
-                React Developer
-              </td>
-              <td class="px-6 py-4">
-                <div class="flex items-center">
-                  <div class="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
-                </div>
-              </td>
-              <td class="px-6 py-4">
-                <!-- Modal toggle -->
-                <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-              </td>
-            </tr>
+                <td class="px-6 py-4">
+                  React Developer
+                </td>
+                <td class="px-6 py-4">
+                  <div class="flex items-center">
+                    <div class="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
+                  </div>
+                </td>
+                <td class="px-6 py-4">
+                  <!-- Modal toggle -->
+                  <a href="#" type="button" data-modal-target="editUserModal" data-modal-show="editUserModal"
+                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
+                </td>
+              </tr>
+            @endforeach
           </tbody>
         </table>
         <!-- Edit user modal -->
